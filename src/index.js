513c9758d1eaa68f4668b436437b939c01e55aeb
Ethan May <ethaniel.may@gmail.com>
2021-07-20 22:26:11 -0600

    Game Constructor

--- src/index.js
+++ src/index.js
@@ -11,13 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
@@ -33,8 +26,9 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return (
-      <Square value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)} 
+      <Square 
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)} 
       />
     );
   }
@@ -72,6 +66,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
